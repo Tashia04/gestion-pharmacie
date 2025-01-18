a430b1d8764eb715c6d40ac83c91b1653e82749d
Tashia04 <tatafatim30@gmail.com>
2025-01-18 14:13:48 +0000

    Update Layout.php

--- partials/Layout.php
+++ partials/Layout.php
@@ -1,4 +1,6 @@
 <?php require_once "./partials/header.php" ?>
-  <?php require_once "./partials/Sidebar.php" ?>
-  <?php require_once $app_content  ?>
+<?php require_once "./partials/Sidebar.php" ?>
+<?php require_once $app_content  ?>
 <?php require_once "./partials/footer.php" ?>
+
+<!-- hello -->
